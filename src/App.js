59ad396ebda8903c83613db989bd93ba500c5e5a
tamim08cse <tamimhasan08@gmail.com>
2022-01-27 23:49:13 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+   return (
+      <div>
+         <h1>Hello World</h1>
+         <p>This is my first react app</p>
+      </div>
+   );
+}
+
+export default App;
